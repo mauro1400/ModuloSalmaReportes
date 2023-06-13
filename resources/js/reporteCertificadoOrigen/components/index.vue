8ab39e4bbe92337b2a7d267e5cf783a5b9e2fb57
mauro silva <mauro1400@outlook.es>
2023-06-13 16:24:36 -0400

    actualizacion ReporteCertificadoOrigenController

--- resources/js/reporteCertificadoOrigen/components/index.vue
+++ resources/js/reporteCertificadoOrigen/components/index.vue
@@ -2,95 +2,105 @@
     <div class="container-fluid">
         <section class="gradient-custom">
             <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
-                <div class="card-body p-4 p-md-4">
-                    <h3>Reporte Certificados de Origen</h3>
-                    <hr>
-                    <form @submit.prevent="submitForm">
-                        <div class="row">
-                            <div class="col-md-2 mb-4">
-                                <label class="form-label" for="regional">Regional</label>
-                                <select class="form-control form-control-sm" id="regional" v-model="regional">
-                                    <option value="">Seleccione Regional</option>
-                                    <option v-for="option in regionales" :value="option.name">{{ option.name }}</option>
-                                </select>
-                            </div>
-                            <div class="col-md-2 mb-4">
-                                <label class="form-label" for="primer_apellido">Fecha Inicio</label>
-                                <input class="form-control form-control-sm" type="text" id="fachainicio"
-                                    v-model="fechainicio" placeholder="Ej. año-mes-dia">
-                            </div>
-                            <div class="col-md-2 mb-4">
-                                <label class="form-label" for="primer_apellido">Fecha Fin</label>
-                                <input class="form-control form-control-sm" type="text" id="fechafin" v-model="fechafin"
-                                    placeholder="Ej. año-mes-dia">
-                            </div>
-                            <div class="col-md-2 mb-4">
-                                <label class="form-label" for="ci">Servidor Publico</label>
-                                <select class="form-control form-control-sm" id="servidor" v-model="solicitante">
-                                    <option value="">Seleccione Servidor Publico</option>
-                                    <option v-for="option in solicitantes" :value="option.name">{{ option.name }}</option>
-                                </select>
-                            </div>
-                            <div class="col-md-2 mb-4">
-                                <label class="form-label" for="celular">Tipo Certificado</label>
-                                <select class="form-control form-control-sm" id="certificado" v-model="certificado">
-                                    <option value="">Seleccione Tipo de Certificado</option>
-                                    <option v-for="option in certificados" :value="option.description">{{ option.description
-                                    }}</option>
-                                </select>
-                            </div>
-                            <div class="col-md-2 mb-4">
-                                <br>
-                                <button type="submit" class="btn btn-outline-success"><i
-                                        class="fa-sharp fa-solid fa-magnifying-glass"></i></button>
-                                <button type="button" class="btn btn-outline-danger" @click="resetForm"><i
-                                        class="fa-solid fa-trash-can"></i></button>
-                            </div>
+                <h3 class="card-header">Reporte Certificados de Origen</h3>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-11 mb-4">
+                            <form @submit.prevent="submitForm">
+                                <div class="row">
+                                    <div class="col-md-2 mb-4" style="margin-top: 24px;">
+                                        <label class="form-label" for="regional">Regional</label>
+                                        <select class="form-control form-control-sm" id="regional" v-model="regional">
+                                            <option value="">Seleccione Regional</option>
+                                            <option v-for="option in regionales" :value="option.name">{{ option.name }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-2 mb-4" style="margin-top: 24px;">
+                                        <label class="form-label" for="primer_apellido">Fecha Inicio</label>
+                                        <input class="form-control form-control-sm" type="text" id="fachainicio"
+                                            v-model="fechainicio" placeholder="Ej. año-mes-dia">
+                                    </div>
+                                    <div class="col-md-2 mb-4" style="margin-top: 24px;">
+                                        <label class="form-label" for="primer_apellido">Fecha Fin</label>
+                                        <input class="form-control form-control-sm" type="text" id="fechafin"
+                                            v-model="fechafin" placeholder="Ej. año-mes-dia">
+                                    </div>
+                                    <div class="col-md-2 mb-4" style="margin-top: 24px;">
+                                        <label class="form-label" for="ci">Servidor Publico</label>
+                                        <select class="form-control form-control-sm" id="servidor" v-model="solicitante">
+                                            <option value="">Seleccione Servidor Publico</option>
+                                            <option v-for="option in solicitantes" :value="option.name">{{ option.name }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-2 mb-4" style="margin-top: 24px;">
+                                        <label class="form-label" for="celular">Tipo Certificado</label>
+                                        <select class="form-control form-control-sm" id="certificado" v-model="certificado">
+                                            <option value="">Seleccione Tipo de Certificado</option>
+                                            <option v-for="option in certificados" :value="option.description">{{
+                                                option.description
+                                            }}</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-2 mb-4 d-flex justify-content-end align-items-center"
+                                        style="margin-top: 24px;">
+                                        <button type="submit" class="btn btn-outline-success"><i
+                                                class="fa-sharp fa-solid fa-magnifying-glass"></i></button><span
+                                            style="margin: 0 10px;"></span>
+                                        <button type="button" class="btn btn-outline-danger" @click="resetForm"><i
+                                                class="fa-solid fa-trash-can"></i></button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                    <button class="btn btn-outline-success" @click="reporte()"><i
-                            class="fa-regular fa-file-excel"></i></button>
+                        <div class="col-md-1 mb-4 d-flex justify-content-start align-items-center">
+                            <button class="btn btn-outline-success" @click="reporte()"><i
+                                    class="fa-regular fa-file-excel"></i></button>
+                        </div>
+                    </div>
                 </div>
-                <hr>
-                <template v-if="busquedas.length === 0">
-                    <no-hay-resultados></no-hay-resultados>
-                </template>
-                <template v-else>
-                    <table class="table table-sm small-font">
-                        <thead class="table-primary">
-                            <tr>
-                                <th>#</th>
-                                <th>Fecha de Entrega</th>
-                                <th>Nro Solicitud</th>
-                                <th>Solicitante </th>
-                                <th>Administrador</th>
-                                <th>Departamento</th>
-                                <th>Articulo</th>
-                                <th>Block Certificado</th>
-                                <th>Entregado</th>
-                                <th>Observacion</th>
-                                <th>Del</th>
-                                <th>Al</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(item, index) in busquedas" :key="index">
-                                <td>{{ index + 1 }}</td>
-                                <td>{{ item.fecha_entrega }}</td>
-                                <td>{{ item.nro_solicitud }}</td>
-                                <td>{{ item.solicitante }}</td>
-                                <td>{{ item.administrador }}</td>
-                                <td>{{ item.departamento }}</td>
-                                <td>{{ item.articulo }}</td>
-                                <td>{{ item.pedido }}</td>
-                                <td>{{ item.entregado }}</td>
-                                <td>{{ item.observacion }}</td>
-                                <td>{{ item.del }}</td>
-                                <td>{{ item.al }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </template>
+                <div class="card-footer">
+                    <template v-if="busquedas.length === 0">
+                        <no-hay-resultados></no-hay-resultados>
+                    </template>
+                    <template v-else>
+                        <table class="table table-sm small-font">
+                            <thead class="table-primary">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Fecha de Entrega</th>
+                                    <th>Nro Solicitud</th>
+                                    <th>Solicitante </th>
+                                    <th>Administrador</th>
+                                    <th>Departamento</th>
+                                    <th>Articulo</th>
+                                    <th>Block Certificado</th>
+                                    <th>Entregado</th>
+                                    <th>Observacion</th>
+                                    <th>Del</th>
+                                    <th>Al</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(item, index) in busquedas" :key="index">
+                                    <td>{{ index + 1 }}</td>
+                                    <td>{{ item.fecha_entrega }}</td>
+                                    <td>{{ item.nro_solicitud }}</td>
+                                    <td>{{ item.solicitante }}</td>
+                                    <td>{{ item.administrador }}</td>
+                                    <td>{{ item.departamento }}</td>
+                                    <td>{{ item.articulo }}</td>
+                                    <td>{{ item.pedido }}</td>
+                                    <td>{{ item.entregado }}</td>
+                                    <td>{{ item.observacion }}</td>
+                                    <td>{{ item.del }}</td>
+                                    <td>{{ item.al }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </template>
+                </div>
             </div>
         </section>
     </div>
@@ -182,9 +192,9 @@ export default {
         },
         resetForm() {
             this.regional = '';
-            this.fachaInicio = '';
-            this.fechaFin = '';
-            this.servidor = '';
+            this.fechainicio = '';
+            this.fechafin = '';
+            this.solicitante = '';
             this.certificado = '';
             this.busquedas = [];
         },
